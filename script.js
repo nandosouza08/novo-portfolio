656aa4bbb12e6821d5c51cb6ac89b1d73f8d223e
nandosouza08 <nandosk8scastro@gmail.com>
2022-09-25 04:39:35 -0300

    adição de imagens

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var btn = document.querySelector("#back-to-top");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
+const btn = document.getElementById("btnTop")
+
+btn.addEventListener("click", function(){
+    window.scroll(0,0)
+})
+
+
+document.addEventListener('scroll',ocultar)
+
+function ocultar() {
+    if(window.scrollY > 980) {
+        btn.style.display = "flex"
+    }else {
+        btn.style.display = "none"
+    }
+}
+
+ocultar()
