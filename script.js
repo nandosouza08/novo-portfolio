5074396f15d5fce1f6a0d9ed74b7567035af2b43
nandosouza08 <nandosk8scastro@gmail.com>
2022-09-25 12:07:48 -0300

    inclusão de efeitos com js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BOTÃO FLUTUANTE SCROLL TO TOP
+
 const btn = document.getElementById("btnTop")
 
 btn.addEventListener("click", function(){
@@ -16,3 +18,29 @@ function ocultar() {
 }
 
 ocultar()
+
+
+// SCROLL TO ITEM
+
+/* ANIMAÇÃO SMOOTH PARA DESKTOP */
+
+const menuItems = document.querySelectorAll('.cabecalho-menu a[href^="#"]');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+function scrollToIdOnClick (event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) -8;
+    scrollToPosition(to);
+}
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
